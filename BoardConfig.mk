8d6bded38fe0848862962bdb9ae0637b16999618
Tri <67373913+TriDiscord@users.noreply.github.com>
2022-01-11 18:21:21 +0300

    twrp: gr8nole patches

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,80 +1,96 @@
 # Platform
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_BOARD_PLATFORM := mrvl
-TARGET_BOARD_PLATFORM_GPU := vivante-gc1000
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a9
-TARGET_CPU_SMP := true
-TARGET_SOC := pxa988
-ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_BOOTLOADER_BOARD_NAME := pxa988
-BOARD_VENDOR := samsung
+TARGET_ARCH                  := arm
+TARGET_ARCH_VARIANT          := armv7-a-neon
+TARGET_BOARD_PLATFORM        := mrvl
+TARGET_BOARD_PLATFORM_GPU    := vivante-gc1000
+TARGET_CPU_ABI               := armeabi-v7a
+TARGET_CPU_ABI2              := armeabi
+TARGET_CPU_VARIANT           := cortex-a9
+TARGET_CPU_SMP               := true
+TARGET_SOC                   := pxa988
+ARCH_ARM_HAVE_TLS_REGISTER   := true
+TARGET_BOOTLOADER_BOARD_NAME := PXA988
+BOARD_VENDOR                 := samsung
 
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CFLAGS   += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
 
 # U-boot
 TARGET_NO_BOOTLOADER := true
-#TARGET_NO_RADIOIMAGE := true
+TARGET_NO_RADIOIMAGE := true
 
 # Kernel
-TARGET_PREBUILT_KERNEL := device/samsung/goya/prebuilt/zImage
-BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive androidboot.hardware=pxa988
-BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_OFFSET := 0x00008000
-BOARD_RAMDISK_OFFSET := 0x01000000
-BOARD_SECOND_OFFSET := 0x00f00000
+TARGET_PREBUILT_KERNEL   := device/samsung/goya/prebuilt/zImage
+BOARD_KERNEL_CMDLINE     := androidboot.selinux=permissive androidboot.hardware=pxa988
+BOARD_KERNEL_BASE        := 0x10000000
+BOARD_KERNEL_OFFSET      := 0x00008000
+BOARD_RAMDISK_OFFSET     := 0x01000000
+BOARD_SECOND_OFFSET      := 0x00f00000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --cmdline "androidboot.selinux=permissive androidboot.hardware=pxa988"
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/goya/mkbootimg.mk
-BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS     := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --cmdline "androidboot.selinux=permissive androidboot.hardware=pxa988"
+BOARD_CUSTOM_BOOTIMG_MK  := device/samsung/goya/mkbootimg.mk
+BOARD_KERNEL_PAGESIZE    := 2048
 
 # Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1594884096
-BOARD_CACHEIMAGE_PARTITION_SIZE := 360710144
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 1594884096
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 360710144
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 5509218304
-BOARD_FLASH_BLOCK_SIZE := 4096
-TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_FLASH_BLOCK_SIZE             := 4096
+TARGET_USERIMAGES_USE_EXT4         := true
+BOARD_HAS_NO_MISC_PARTITION        := true
 
 # Update OTA
 BLOCK_BASED_OTA := false
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := goya,goyawifi,goya3g
+TARGET_OTA_ASSERT_DEVICE   := goya,goyawifi,goya3g
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 # TWRP
-TW_THEME := portrait_mdpi
-TW_MTP_DEVICE := "/dev/mtp_usb"
+BOARD_USES_MMCUTILS        := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_RECOVERY_SWIPE       := true
+RECOVERY_FSTAB_VERSION     := 2
+TARGET_RECOVERY_FSTAB      := device/samsung/goya/rootdir/fstab.pxa988
+
+TW_THEME                     := portrait_mdpi
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
-TARGET_USES_LOGD := true
+TARGET_USES_LOGD             := true
 
 TARGET_RECOVERY_INITRC := device/samsung/goya/rootdir/init.rc
 
 RECOVERY_GRAPHICS_FORCE_SINGLE_BUFFER := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_GRAPHICS_USE_LINELENGTH      := true
 
 TW_NO_REBOOT_BOOTLOADER := true
-TW_HAS_DOWNLOAD_MODE := true
+TW_HAS_DOWNLOAD_MODE    := true
+
+TW_INTERNAL_STORAGE_PATH        := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH        := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+BOARD_HAS_NO_REAL_SDCARD        := true
+SP1_NAME                        := "efs"
+SP1_BACKUP_METHOD               := files
 
 TW_EXCLUDE_TWRPAPP      := true
-#TW_EXCLUDE_MTP          := false
+TW_EXCLUDE_MTP          := false
+TW_NO_USB_STORAGE       := true
+TW_INCLUDE_FUSE_EXFAT   := true
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone1/temp"
 TW_USE_TOOLBOX          := true
 
 RECOVERY_SDCARD_ON_DATA := true
 
 # MRVL hardware + Graphics
-BOARD_USES_MRVL_HARDWARE := true
-MRVL_ION := true
-MRVL_INTERFACE_ANIMATION := true
-TARGET_FORCE_CPU_UPLOAD := true
-USE_OPENGL_RENDERER := true
-BOARD_USE_BGRA_8888 := true
+BOARD_USES_MRVL_HARDWARE     := true
+MRVL_ION                     := true
+MRVL_INTERFACE_ANIMATION     := true
+TARGET_FORCE_CPU_UPLOAD      := true
+USE_OPENGL_RENDERER          := true
+BOARD_USE_BGRA_8888          := true
 BOARD_HAVE_PIXEL_FORMAT_INFO := true
 
 # Resolution
@@ -82,5 +98,6 @@ TARGET_SCREEN_HEIGHT := 1024
 TARGET_SCREEN_WIDTH  := 600
 
 # Charging mode
-BOARD_LPM_BOOT_ARGUMENT_NAME := lpm_boot
+BOARD_LPM_BOOT_ARGUMENT_NAME  := lpm_boot
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 BOARD_LPM_BOOT_ARGUMENT_VALUE := 1
--- omni_goya.mk
+++ omni_goya.mk
@@ -4,13 +4,20 @@ $(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, build/target/product/full_base_telephony.mk)
 $(call inherit-product, build/target/product/languages_small.mk)
 
+# Enable higher-res drawables while keeping mdpi as primary source
+PRODUCT_AAPT_CONFIG      := large mdpi hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+PRODUCT_LOCALES          += mdpi
+
 PRODUCT_COPY_FILES += device/samsung/goya/prebuilt/zImage:kernel
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := goya
-PRODUCT_NAME := omni_goya
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-T110
+PRODUCT_DEVICE       := goya
+PRODUCT_NAME         := omni_goya
+PRODUCT_BRAND        := samsung
+PRODUCT_MODEL        := SM-T110
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_RELEASE_NAME := Samsung Galaxy Tab 3 Lite 7.0
-TARGET_VENDOR := samsung
+TARGET_VENDOR        := samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
