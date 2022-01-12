5f245e6b963cae8d00fa741a7ae4ca2ac3ceeb66
Tri <67373913+TriDiscord@users.noreply.github.com>
2022-01-12 19:16:51 +0300

    fstab: remove pxa988 config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,12 +62,13 @@ BOARD_USES_MMCUTILS        := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_RECOVERY_SWIPE       := true
 RECOVERY_FSTAB_VERSION     := 1
-TARGET_RECOVERY_FSTAB      := device/samsung/goya/rootdir/fstab.pxa988
+TARGET_RECOVERY_FSTAB      := device/samsung/goya/recovery.fstab
 
 
 TW_THEME                     := landscape_mdpi
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 TARGET_USES_LOGD             := true
+TWRP_INCLUDE_LOGCAT          := true
 
 TARGET_RECOVERY_INITRC := device/samsung/goya/rootdir/init.rc
 
