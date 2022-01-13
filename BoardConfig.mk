c85eaa734fb81e5365e27538a92dcb6f918ed30e
Tri <67373913+TriDiscord@users.noreply.github.com>
2022-01-13 23:44:38 +0300

    BoardConfig: Fix all

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,7 +67,7 @@ TARGET_RECOVERY_FSTAB      := device/samsung/goya/recovery.fstab
 BOARD_UMS_LUNFILE          := "/sys/class/android_usb/f_mass_storage/lun/file"
 #TW_CUSTOM_CPU_TEMP_PATH    := "/sys/class/thermal/thermal_zone0/temp"
 
-TW_THEME                     := portrait_hdpi
+TW_THEME                     := landscape_mdpi
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 
 TARGET_RECOVERY_INITRC      := device/samsung/goya/recovery/root/init.rc
@@ -93,7 +93,7 @@ SP1_BACKUP_METHOD               := files
 TW_EXCLUDE_TWRPAPP      := true
 TW_USE_TOOLBOX          := false
 TW_NO_USB_STORAGE       := true
-TW_EXCLUDE_MTP          := false
+TW_EXCLUDE_MTP := true
 TW_INCLUDE_FUSE_EXFAT   := true
 TW_INCLUDE_CRYPTO       := false
 TW_NO_CPU_TEMP          := true
