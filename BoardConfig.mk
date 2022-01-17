f35cabaa66aeab9ed32624f1aba28533efda98fe
Tri <67373913+TriDiscord@users.noreply.github.com>
2022-01-17 08:43:31 +0300

    BoardConfig: MRVL patches

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,26 +91,28 @@ BOARD_HAS_NO_REAL_SDCARD        := true
 SP1_NAME                        := "efs"
 SP1_BACKUP_METHOD               := files
 
-TW_EXCLUDE_TWRPAPP          := true
-TW_USE_TOOLBOX              := false
-TW_NO_USB_STORAGE           := true
-TW_EXCLUDE_DEFAULT_USB_INIT := false
-TW_INCLUDE_FUSE_EXFAT       := true
-TW_INCLUDE_CRYPTO           := false
-TW_NO_CPU_TEMP              := true
-TW_EXTRA_LANGUAGES          := true
+TW_EXCLUDE_TWRPAPP           := true
+TW_USE_TOOLBOX               := false
+TW_EXCLUDE_DEFAULT_USB_INIT  := true
+TW_INCLUDE_FUSE_EXFAT        := true
+TW_INCLUDE_CRYPTO            := false
+TW_EXCLUDE_ENCRYPTED_BACKUPS := true
+TW_NO_CPU_TEMP               := true
+TW_EXTRA_LANGUAGES           := true
 
 RECOVERY_SDCARD_ON_DATA := false
 
 
 # MRVL hardware + Graphics
-BOARD_USES_MRVL_HARDWARE     := true
-MRVL_ION                     := true
-MRVL_INTERFACE_ANIMATION     := true
-TARGET_FORCE_CPU_UPLOAD      := true
-USE_OPENGL_RENDERER          := true
-BOARD_USE_BGRA_8888          := true
-BOARD_HAVE_PIXEL_FORMAT_INFO := true
+BOARD_USES_MRVL_HARDWARE          := true
+MRVL_ION                          := true
+MRVL_INTERFACE_ANIMATION          := true
+TARGET_FORCE_CPU_UPLOAD           := true
+USE_OPENGL_RENDERER               := true
+BOARD_USE_BGRA_8888               := true
+BOARD_HAVE_PIXEL_FORMAT_INFO      := true
+BOARD_USE_VIVANTE_GRALLOC         := true
+MRVL_LAUNCH_DMS_IN_SURFACEFLINGER := true
 
 
 # Resolution
