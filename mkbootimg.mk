084753bc9a434cd703fff4abcc7e4c8837d809ee
Tri <67373913+TriDiscord@users.noreply.github.com>
2022-01-30 19:57:17 +0300

    mkbootimg: Update from 7.1 hlte

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,17 +1,24 @@
+BUILT_RAMDISK_CPIO := $(PRODUCT_OUT)/ramdisk-recovery.cpio
 COMPRESS_COMMAND := xz --format=lzma
-LOCAL_PATH := $(call my-dir)
 
-FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
+ifdef TARGET_PREBUILT_DTB
+	BOARD_MKBOOTIMG_ARGS += --dt $(TARGET_PREBUILT_DTB)
+endif
 
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
-	$(call pretty,"Target boot image: $@")
-	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
-	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
-
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) 		$(recovery_ramdisk) 		$(recovery_kernel)
-	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
-	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
-	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
+INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(recovery_ramdisk)
+	@echo -e ${CL_CYN}"------- Compressing recovery ramdisk -------"${CL_RST}
+	$(hide) $(COMPRESS_COMMAND) "$(BUILT_RAMDISK_CPIO)"
+	@echo -e ${CL_CYN}"------- Making recovery image -------"${CL_RST}
+	$(hide) $(MKBOOTIMG) \
+		--kernel $(TARGET_PREBUILT_KERNEL) \
+		--ramdisk $(BUILT_RAMDISK_CPIO).lzma \
+		--cmdline "$(BOARD_KERNEL_CMDLINE)" \
+		--base $(BOARD_KERNEL_BASE) \
+		--pagesize $(BOARD_KERNEL_PAGESIZE) \
+		$(BOARD_MKBOOTIMG_ARGS) \
+		-o $(INSTALLED_RECOVERYIMAGE_TARGET)
+	@echo -e ${CL_CYN}"------- Made recovery image: $@ -------"${CL_RST}
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	$(hide) tar -C $(PRODUCT_OUT) -H ustar -c recovery.img > $(FLASH_IMAGE_TARGET)
-	@echo -e ${CL_CYN}"Made Odin-flashable recovery.tar: ${FLASH_IMAGE_TARGET}"${CL_RST}
+	@echo -e ${CL_CYN}"------- Made Odin-flashable recovery.tar: ${FLASH_IMAGE_TARGET} -------"${CL_RST}
